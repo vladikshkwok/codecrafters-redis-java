724027b29ab0abd8e07d8c596838ef8debaa79a5
Vladislav <vladikshwok@gmail.com>
2024-11-03 18:27:49 +0300

    Small refactoring

--- src/main/java/ru/vladikshk/myRedis/service/SimpleStorageService.java
+++ src/main/java/ru/vladikshk/myRedis/service/SimpleStorageService.java
@@ -8,10 +8,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class SimpleStorageService implements StorageService {
     private final Map<String, String> storageMap;
+    private final WatchdogService watchdogService;
 
     public SimpleStorageService() {
         this.storageMap = new ConcurrentHashMap<>();
-        new SimpleWatchDogService(this);
+        this.watchdogService = new SimpleWatchDogService(this);
     }
 
     @Override
@@ -22,6 +23,7 @@ public class SimpleStorageService implements StorageService {
     @Override
     public void put(String key, String value, int expireMs) {
         storageMap.put(key, value);
+        watchdogService.addForWatch(key, expireMs);
     }
 
     @Override
--- src/main/java/ru/vladikshk/myRedis/service/SimpleWatchDogService.java
+++ src/main/java/ru/vladikshk/myRedis/service/SimpleWatchDogService.java
@@ -41,7 +41,7 @@ public class SimpleWatchDogService implements WatchdogService {
                     throw new RuntimeException(e);
                 }
             }
-        });
+        }).start();
     }
 
     @Override
