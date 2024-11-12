74cb2e5050d667e4e04fb3163d9fdcd5971af60e
vladikshk <vladikshkwok@gmail.com>
2024-11-12 18:24:08 +0300

    ACKs with no commands

--- src/main/java/ru/vladikshk/myRedis/service/SimpleReplicationService.java
+++ src/main/java/ru/vladikshk/myRedis/service/SimpleReplicationService.java
@@ -8,7 +8,6 @@ import ru.vladikshk.myRedis.types.RArray;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,8 +18,6 @@ public class SimpleReplicationService implements ReplicationService {
     private final RedisConfig redisConfig = RedisConfig.getInstance();
     private Set<ReplicaConnection> replicas;
 
-    private BufferedReader in;
-    private OutputStream out;
 
     private SimpleReplicationService() {
         replicas = new HashSet<>();
@@ -46,10 +43,10 @@ public class SimpleReplicationService implements ReplicationService {
                 throw new IllegalStateException("Couldn't connect to master on " + host);
             }
 
-            in = new BufferedReader(new InputStreamReader(masterConnection.getInputStream()));
-            out = new BufferedOutputStream(masterConnection.getOutputStream());
+            var in = new BufferedReader(new InputStreamReader(masterConnection.getInputStream()));
+            var out = new BufferedOutputStream(masterConnection.getOutputStream());
 
-            sendHandShake();
+            sendHandShake(out, in);
             return masterConnection;
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -81,24 +78,23 @@ public class SimpleReplicationService implements ReplicationService {
         });
     }
 
-    private void sendHandShake() throws IOException {
+    private void sendHandShake(OutputStream out1, BufferedReader in1) throws IOException {
         log.info("Sending ping to master redis");
-        out.write(new RArray(List.of("PING")).getBytes());
-        out.flush();
-        log.info("response: {}", in.readLine());
+        out1.write(new RArray(List.of("PING")).getBytes());
+        out1.flush();
+        log.info("response: {}", in1.readLine());
         log.info("Sending replconf to master redis with listening port");
-        out.write(new RArray(List.of("REPLCONF", "listening-port", redisConfig.getPort().toString())).getBytes());
-        out.flush();
-        log.info("response: {}", in.readLine());
+        out1.write(new RArray(List.of("REPLCONF", "listening-port", redisConfig.getPort().toString())).getBytes());
+        out1.flush();
+        log.info("response: {}", in1.readLine());
         log.info("Sending replconf to master redis with capabilities");
-        out.write(new RArray(List.of("REPLCONF", "capa", "sync")).getBytes());
-        out.flush();
-        log.info("response: {}", in.readLine());
+        out1.write(new RArray(List.of("REPLCONF", "capa", "sync")).getBytes());
+        out1.flush();
+        log.info("response: {}", in1.readLine());
         log.info("Sending psync to master redis");
-        out.write(new RArray(List.of("PSYNC", "?", "-1")).getBytes());
-        out.flush();
-        log.info("response: {}", in.readLine());
-        in.readLine();
+        out1.write(new RArray(List.of("PSYNC", "?", "-1")).getBytes());
+        out1.flush();
+        log.info("response: {}", in1.readLine());
     }
 
 }
